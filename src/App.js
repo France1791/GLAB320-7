4186ddcf1335e918b7cf16998668187a40a1819f
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-09-06 12:21:09 -0400

    finished work for the movie search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// Import our components
+import MovieDisplay from "./Components/MovieDisplay";
+import Form from "./Components/Form";
 
-function App() {
+
+export default function App() {
+  // Constant with your API Key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    // Make fetch request and store the response
+    try{
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      // Set the Movie state to the received data
+      setMovie(data);
+    } catch (error) {
+      console.error(error);
+    }
+   
+  };
+
+   // This will run on the first render but not on subsquent renders
+   useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+  // We pass the getMovie function as a prop called moviesearch
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie}/>
     </div>
   );
 }
 
-export default App;
+
